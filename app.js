8ecbff1fa294f8db974e46308c75e854b6173415
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-07-23 22:42:46 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+// student ={
+
+// name:"Abdul.Moiz",
+// age:17,
+// course:"web developement",
+// marks:82.3,
+// Fathername:"Muhammad Dilawar",
+// };
+
+
+
+// let arr=["Apple","Mango","Banana"];
+
+// class Toyotacar{
+
+// constructor(brand,mileage){
+// console.log("creating new object");
+// this.brandName = brand;
+// this.mileage = mileage;
+// }     
+// start(){
+// console.log("start");
+// }
+
+// stop(){
+//     console.log("stop");
+// }
+// }
+ 
+// let fortuner=new Toyotacar("fortuner",10);
+// console.log(fortuner);
+// let lexus = new Toyotacar("lexus",12);
+// console.log(lexus);
+
+
+
+
