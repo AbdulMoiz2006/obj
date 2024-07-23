f12f447150e06a77b4f8101cf906f7dd95518bb6
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-07-23 22:46:52 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
-// student ={
+ student ={
 
-// name:"Abdul.Moiz",
-// age:17,
-// course:"web developement",
-// marks:82.3,
-// Fathername:"Muhammad Dilawar",
-// };
+ name:"Abdul.Moiz",
+ age:17,
+ course:"web developement",
+ marks:82.3,
+ Fathername:"Muhammad Dilawar",
+ };
 
 
 
-// let arr=["Apple","Mango","Banana"];
+ let arr=["Apple","Mango","Banana"];
 
-// class Toyotacar{
+ class Toyotacar{
 
-// constructor(brand,mileage){
-// console.log("creating new object");
-// this.brandName = brand;
-// this.mileage = mileage;
-// }     
-// start(){
-// console.log("start");
-// }
+ constructor(brand,mileage){
+ console.log("creating new object");
+this.brandName = brand;
+this.mileage = mileage;
+ }     
+ start(){
+ console.log("start");
+ }
 
-// stop(){
-//     console.log("stop");
-// }
-// }
+ stop(){
+   console.log("stop");
+ }
+ }
  
-// let fortuner=new Toyotacar("fortuner",10);
-// console.log(fortuner);
-// let lexus = new Toyotacar("lexus",12);
-// console.log(lexus);
+ let fortuner=new Toyotacar("fortuner",10);
+ console.log(fortuner);
+ let lexus = new Toyotacar("lexus",12);
+console.log(lexus);
 
 
 
